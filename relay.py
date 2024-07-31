736fce22e907f7d9172b2a8b709d0bdcda758f9f
steven <zx4567zx4567@gmail.com>
2024-07-31 09:30:40 +0800

    :q

--- relay.py
+++ relay.py
@@ -45,7 +45,45 @@ def reboot(index):
      time.sleep(2)
      GPIO.output(n4Pin, GPIO.LOW)
 
-opts, args = getopt.getopt(sys.argv[1:], '-h-f:-v-i-r', ['help', 'filename=','version', 'init', 'reboot='])
+
+def shutdown(index):
+  print("shutdown device on relay %s" % index)
+  GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+  GPIO.setup(n1Pin, GPIO.OUT)
+  GPIO.setup(n2Pin, GPIO.OUT)
+  GPIO.setup(n3Pin, GPIO.OUT)
+  GPIO.setup(n4Pin, GPIO.OUT)
+  if index in ('1', 'n1'):
+     GPIO.output(n1Pin, GPIO.HIGH)
+  if index in ('2', 'n2'):
+     GPIO.output(n2Pin, GPIO.HIGH)
+  if index in ('3', 'n3'):
+     GPIO.output(n3Pin, GPIO.HIGH)
+  if index in ('4', 'n4'):
+     GPIO.output(n4Pin, GPIO.HIGH)
+
+def poweron(index):
+  print("shutdown device on relay %s" % index)
+  GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+  GPIO.setup(n1Pin, GPIO.OUT)
+  GPIO.setup(n2Pin, GPIO.OUT)
+  GPIO.setup(n3Pin, GPIO.OUT)
+  GPIO.setup(n4Pin, GPIO.OUT)
+  if index in ('1', 'n1'):
+     GPIO.output(n1Pin, GPIO.LOW)
+  if index in ('2', 'n2'):
+     GPIO.output(n2Pin, GPIO.LOW)
+  if index in ('3', 'n3'):
+     GPIO.output(n3Pin, GPIO.LOW)
+  if index in ('4', 'n4'):
+     GPIO.output(n4Pin, GPIO.LOW)
+
+
+
+
+opts, args = getopt.getopt(sys.argv[1:], '-h-f:-v-i-r-s-p', ['help', 'filename=','version', 'init', 'reboot=', 'shutdown=', 'poweron='])
 for opt_name, opt_value in opts:
   if opt_name in ('-i', '--init'):
       print("init pin")
@@ -53,6 +91,12 @@ for opt_name, opt_value in opts:
       exit()
   if opt_name in ('-r', '--reboot'):
       reboot(opt_value)
+      exit()
+  if opt_name in ('-s', '--shutdown'):
+      shutdown(opt_value)
+      exit()
+  if opt_name in ('-p', '--poweron'):
+      poweron(opt_value)
       exit()
   if opt_name in ('-h', '--help'):
       print("help info")
